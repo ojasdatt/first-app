43d5d37b1662aea7c5cab030f77514534f82fdde
ojasdatt <ojadatt@gmail.com>
2026-03-14 13:05:58 -0400

    Homework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Customer Name</th>
+          <th>Customer City</th>
+          <th>Amount of Purchase</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Praveen</td>
+          <td>Madurai</td>
+          <td>12000</td>
+        </tr>
+        <tr>
+          <td>Kumar</td>
+          <td>Chennai</td>
+          <td>10000</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
---
+++ src/expressions.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const myelement = <h1>My score is {10*9}</h1>;
+
+ReactDOM.render(myelemnt, document.getElementById('root'));

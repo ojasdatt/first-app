33afe71ebdccd593d19a7b381873b8d10d858188
ojasdatt <ojadatt@gmail.com>
2026-03-28 12:49:19 -0400

    firstapp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import Goal from './Details';
+import ReactDOM from 'react-dom/client';
+class Customer extends React.Component {
+  render() {
+    return <h2>I am from {this.props.city}! </h2>
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Hi, I am Aaryan</h2>
-        <Goal />
-      </div>
-    );
+    <div>
+    <h1>Hello</h1>
+    <Customer city="India" />
+    </div>
+    )
   }
 }
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />)
 
 export default App;
--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,1 @@
-import React from 'react';
 
-function Goal() {
-  return <h2>My goal is to become a software Engineer</h2>;
-}
-
-export default Goal;

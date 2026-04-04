ab15cea8c4fd47982d86ffb8996aaa29e4eecc32
ojasdatt <ojadatt@gmail.com>
2026-04-04 12:27:50 -0400

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class Food extends React.Component {
-  render() {
-    return <h2>I am {this.props.type} type Food. People love me. I am pizza</h2>
+class Student extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: "Aaryan",
+      grade:6,
+      favourite_color:"green",
+      favourtie_subject: "Math"
+    };
   }
-}
-
-class App extends React.Component {
   render() {
-    const mycode = <Food type="Continental" />;
-    return (
+    return(
     <div>
-    <h1>Hello</h1>
-    {mycode}
+    <h1>Student details</h1>
+    <p>My name is {this.state.name}</p>
+    <p>I am in grade {this.state.grade}</p>
+    <p>My favourite color is{this.state.favourite_color}</p>
+    <p>My favourtie subject is {this.state.favourtie_subject}</p>
     </div>
-    )
-  }
+    );
+    }
 }
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+root.render(<Student />)
 
-export default App;
+export default Student;

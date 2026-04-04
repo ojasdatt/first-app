42cb0832e16397c9759acec361a3b98c2f507659
ojasdatt <ojadatt@gmail.com>
2026-04-04 13:00:26 -0400

    Ayprofile

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class AY extends React.Component {
     name: "Aaryan",
     age: 11,
     favourite_color: "Orange",
-    favourite_subject: "English"
+    favourite_subject: "English",
+    favourite_hobby: "Building"
    };
    }
    Name = () => {
@@ -23,18 +24,23 @@ class AY extends React.Component {
    favouriteSubject = () => {
    this.setState({favourite_subject: "Math"})
    }
+   favouriteHobby = () => {
+   this.setState({favourite_hobby: "Playing video games"})
+   }
    render() {
 return(
 <div>
 <h1>AD/Yd details </h1>
-<p>My name is {this.state.name}</p>
-<p>I am {this.state.age} years old</p>
-<p>my favourite color is {this.state.favourite_color}</p>
-<p>My favourite subject is {this.state.favourite_subject}</p>
-<button type="button" onClick={this.Name}>Click me for name!</button>
-<button type="button" onClick={this.Age}>Click me for age!</button>
-<button type="button" onClick={this.favouriteColor}>Click me for favourite color!</button>
-<button type="button" onClick={this.favouriteSubject}>Click me for favourite subject!</button>
+<p>Ad name is {this.state.name}</p>
+<p>Ad is  {this.state.age} years old</p>
+<p>Ad's favourite color is {this.state.favourite_color}</p>
+<p>Ad's favourite subject is {this.state.favourite_subject}</p>
+<p>Ad's favourite Hobby is {this.state.favourite_hobby}</p>
+<button type="button" onClick={this.Name}>Click me for Yd name!</button>
+<button type="button" onClick={this.Age}>Click me for Yd age!</button>
+<button type="button" onClick={this.favouriteColor}>Click me for Yd favourite color!</button>
+<button type="button" onClick={this.favouriteSubject}>Click me for Yd favourite subject!</button>
+<button type="button" onClick={this.favouriteHobby}>Click me for Yd favourite Hobby!</button>
 </div>
 );
 }

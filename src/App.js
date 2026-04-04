b37d9c3e54cf707ce9fcf70686294dfb5152e9dd
ojasdatt <ojadatt@gmail.com>
2026-04-04 12:40:47 -0400

    2

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 class Student extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      name: "Aaryan",
-      grade:6,
-      favourite_color:"green",
-      favourtie_subject: "Math"
-    };
-  }
-  render() {
-    return(
-    <div>
-    <h1>Student details</h1>
-    <p>My name is {this.state.name}</p>
-    <p>I am in grade {this.state.grade}</p>
-    <p>My favourite color is{this.state.favourite_color}</p>
-    <p>My favourtie subject is {this.state.favourtie_subject}</p>
-    </div>
-    );
-    }
+   constructor(props) {
+   super(props)
+   this.state = {
+    name: "Aaryan",
+    age: 11,
+    favourite_color: "Orange",
+    favourite_subject: "Math"
+   };
+   }
+   favourtieColor = () => {
+   this.setState({favourite_color: "black"});
+   }
+   render() {
+return(
+<div>
+<h1>Student details</h1>
+<p>My name is {this.state.name}</p>
+<p>I am {this.state.age} years old</p>
+<p>my favourite color is {this.state.favourite_color}</p>
+<p>My favourite subject is {this.state.favourite_subject}</p>
+<button type="button" onClick={this.favourtieColor}>Click me</button>
+</div>
+);
+}
 }
 
 const root=ReactDOM.createRoot(document.getElementById('root'));

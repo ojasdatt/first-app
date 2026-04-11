6ac23de44a295a21193293d89e7d4f067de1b70f
ojasdatt <ojadatt@gmail.com>
2026-04-11 12:36:00 -0400

    sync

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+
 
 class MyStyling extends React.Component {
    render() {
-      const myheaderstyle = {
-         color: "orange",
-         backgroundColor: "blue",
-         fontFamily: "Arial",
-         padding: "6 px"
-      };
-
-const mystyle = {
-   color:"red",
-   backgroundColor: "pink",
-   fontFamily: "Times New Roman",
-   padding: "11 px"
-};
-
-return(
-<div>
-<h1 style={myheaderstyle}>Hi</h1>
-<p style={mystyle}>How are you?</p>
-</div>
-);
+      return (
+      <div>
+      <h1 className="myheaderstyle">Hi</h1>
+      <p className="mystyle">How are you?</p>
+      </div>
+      );
    }
 }
+
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MyStyling />)
